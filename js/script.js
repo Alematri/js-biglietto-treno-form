d68f5019c30bf70f7ead04b929af2684ba6e340e
Alematri <alessandromatricardi@gmail.com>
2023-09-18 09:35:16 +0200

    [add] redme, graphic updates, annulla btn

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function stamparisultato(){
   calcolaPrezzo()
   document.getElementById("numerocarro").innerHTML = randomgen("carrozza");
   document.getElementById("codicebiglietto").innerHTML = randomgen("biglietto");
- }
+}
 
 function nomecognome() {
   const name = document.getElementById("nameInput").value;
@@ -25,24 +25,44 @@ function randomgen(tiponumero) {
     max=9999;
     testo = "ID: "
   }
-    return testo + Math.floor(Math.random() * (max - min +1) +min);
+  return testo + Math.floor(Math.random() * (max - min +1) +min);
 }
 
 function calcolaPrezzo() {
   const km = parseInt(document.getElementById("kmInput").value);
-  const age = parseInt(document.getElementById("ageInput").value);
+  const ageSelect = document.getElementById("ageSelect");
+  const selectedValue = ageSelect.options[ageSelect.selectedIndex].value;
   const prezzoAlKm = 0.21;
 
   let prezzoTotale = km * prezzoAlKm;
-  let underage = 0.8;
-  let overage = 0.6;
+  const scontoUnderage = 0.8;
+  const scontoOverage = 0.6;
 
-  if (age < 18) {
-    prezzoTotale *= underage; // Sconto del 20% per i minorenni
-  } else if (age >= 65) {
-    prezzoTotale *= overage; // Sconto del 40% per gli over 65
+  switch (selectedValue) {
+    case "underage":
+      prezzoTotale *= scontoUnderage; // Sconto del 20% per i minorenni
+      break;
+    case "adult":
+      // Nessuno sconto
+      break;
+    case "overage":
+      prezzoTotale *= scontoOverage; // Sconto del 40% per gli over 65
+      break;
   }
 
   const risultato = document.getElementById("risultato");
   risultato.innerHTML = "€" + prezzoTotale.toFixed(2);
 }
+
+// Aggiungi un eventListener al bottone "Genera il tuo biglietto" (btn-gen)
+document.getElementById("btn-gen").addEventListener("click", function() {
+  stamparisultato();
+  // Rimuovi la classe "d-none" dall'elemento resultRow per renderlo visibile
+  document.getElementById("resultRow").classList.remove("d-none");
+});
+
+// Aggiungi un eventListener al bottone "Annulla" (btn-null)
+document.getElementById("btn-null").addEventListener("click", function() {
+  // Aggiungi la classe "d-none" al click dell'elemento resultRow per nasconderlo
+  document.getElementById("resultRow").classList.add("d-none");
+});
